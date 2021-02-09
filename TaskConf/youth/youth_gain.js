4f9b3fdb02766cbb1bb7fabeb2fcfd01f6ac9e7c
gitehabu <78459250+gitehabu@users.noreply.github.com>
2021-02-09 13:36:50 +0800

    Create youth_gain.js

---
+++ TaskConf/youth/youth_gain.js
@@ -0,0 +1,5 @@
+#中青阅读浏览赚
+hostname = *.youth.cn, ios.baertt.com
+
+
+https:\/\/ios\.baertt\.com\/v5\/task\/browse_(start|end)\.json url script-request-body https://raw.githubusercontent.com/gitehabu/Scripts/master/Task/youth_gain.js
